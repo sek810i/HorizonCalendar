4c5fa6a56f05faa89ba6e7ee96491f58ebedb074
sek810i <Sek810i96@gmail.com>
2021-09-01 13:35:04 +0300

    public init Day and Month

--- Sources/Public/Day.swift
+++ Sources/Public/Day.swift
@@ -20,66 +20,66 @@ import Foundation
 /// Represents a day (with a corresponding year and month) in a particular calendar. All days are assumed to have been instantiated
 /// with the same `Calendar`, which is enforced throughout the implementation.
 public struct Day {
-
-  // MARK: Lifecycle
-
-  init(month: Month, day: Int) {
-    self.month = month
-    self.day = day
-  }
-
-  // MARK: Public
-
-  public let month: Month
-  public let day: Int
-
-  public var components: DateComponents {
-    DateComponents(era: month.era, year: month.year, month: month.month, day: day)
-  }
-
+    
+    // MARK: Lifecycle
+    
+    public init(month: Month, day: Int) {
+        self.month = month
+        self.day = day
+    }
+    
+    // MARK: Public
+    
+    public let month: Month
+    public let day: Int
+    
+    public var components: DateComponents {
+        DateComponents(era: month.era, year: month.year, month: month.month, day: day)
+    }
+    
 }
 
 // MARK: CustomStringConvertible
 
 extension Day: CustomStringConvertible {
-
-  public var description: String {
-    let yearDescription = String(format: "%04d", month.year)
-    let monthDescription = String(format: "%02d", month.month)
-    let dayDescription = String(format: "%02d", day)
-    return "\(yearDescription)-\(monthDescription)-\(dayDescription)"
-  }
-
+    
+    public var description: String {
+        let yearDescription = String(format: "%04d", month.year)
+        let monthDescription = String(format: "%02d", month.month)
+        let dayDescription = String(format: "%02d", day)
+        return "\(yearDescription)-\(monthDescription)-\(dayDescription)"
+    }
+    
 }
 
 // MARK: Equatable
 
 extension Day: Equatable {
-
-  public static func == (lhs: Day, rhs: Day) -> Bool {
-    lhs.month == rhs.month && lhs.day == rhs.day
-  }
-
+    
+    public static func == (lhs: Day, rhs: Day) -> Bool {
+        lhs.month == rhs.month && lhs.day == rhs.day
+    }
+    
 }
 
 // MARK: Hashable
 
 extension Day: Hashable {
-
-  public func hash(into hasher: inout Hasher) {
-    hasher.combine(month)
-    hasher.combine(day)
-  }
-
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(month)
+        hasher.combine(day)
+    }
+    
 }
 
 // MARK: Comparable
 
 extension Day: Comparable {
-
-  public static func < (lhs: Day, rhs: Day) -> Bool {
-    guard lhs.month == rhs.month else { return lhs.month < rhs.month }
-    return lhs.day < rhs.day
-  }
-
+    
+    public static func < (lhs: Day, rhs: Day) -> Bool {
+        guard lhs.month == rhs.month else { return lhs.month < rhs.month }
+        return lhs.day < rhs.day
+    }
+    
 }
--- Sources/Public/Month.swift
+++ Sources/Public/Month.swift
@@ -20,82 +20,82 @@ import Foundation
 /// Represents a month (with a corresponding year) in a particular calendar. All months are assumed to have been instantiated with the
 /// same `Calendar`, which is enforced throughout the implementation.
 public struct Month {
-
-  // MARK: Lifecycle
-
-  init(era: Int, year: Int, month: Int, isInGregorianCalendar: Bool) {
-    self.era = era
-    self.year = year
-    self.month = month
-    self.isInGregorianCalendar = isInGregorianCalendar
-  }
-
-  // MARK: Public
-
-  public let era: Int
-  public let year: Int
-  public let month: Int
-
-  public var components: DateComponents {
-    DateComponents(era: era, year: year, month: month)
-  }
-
-  // MARK: Internal
-
-  // In the Gregorian calendar, BCE years (era 0) get larger in descending order (10 BCE < 5 BCE).
-  // This property exists to facilitate an accurate `Comparable` implementation.
-  let isInGregorianCalendar: Bool
-
+    
+    // MARK: Lifecycle
+    
+    public init(era: Int, year: Int, month: Int, isInGregorianCalendar: Bool) {
+        self.era = era
+        self.year = year
+        self.month = month
+        self.isInGregorianCalendar = isInGregorianCalendar
+    }
+    
+    // MARK: Public
+    
+    public let era: Int
+    public let year: Int
+    public let month: Int
+    
+    public var components: DateComponents {
+        DateComponents(era: era, year: year, month: month)
+    }
+    
+    // MARK: Internal
+    
+    // In the Gregorian calendar, BCE years (era 0) get larger in descending order (10 BCE < 5 BCE).
+    // This property exists to facilitate an accurate `Comparable` implementation.
+    let isInGregorianCalendar: Bool
+    
 }
 
 // MARK: CustomStringConvertible
 
 extension Month: CustomStringConvertible {
-
-  public var description: String {
-    "\(String(format: "%04d", year))-\(String(format: "%02d", month))"
-  }
-
+    
+    public var description: String {
+        "\(String(format: "%04d", year))-\(String(format: "%02d", month))"
+    }
+    
 }
 
 // MARK: Equatable
 
 extension Month: Equatable {
-
-  public static func == (lhs: Month, rhs: Month) -> Bool {
-    lhs.era == rhs.era &&
-      lhs.year == rhs.year &&
-      lhs.month == rhs.month &&
-      lhs.isInGregorianCalendar == rhs.isInGregorianCalendar
-  }
-
+    
+    public static func == (lhs: Month, rhs: Month) -> Bool {
+        lhs.era == rhs.era &&
+            lhs.year == rhs.year &&
+            lhs.month == rhs.month &&
+            lhs.isInGregorianCalendar == rhs.isInGregorianCalendar
+    }
+    
 }
 
 // MARK: Hashable
 
 extension Month: Hashable {
-
-  public func hash(into hasher: inout Hasher) {
-    hasher.combine(era)
-    hasher.combine(year)
-    hasher.combine(month)
-    hasher.combine(isInGregorianCalendar)
-  }
-
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(era)
+        hasher.combine(year)
+        hasher.combine(month)
+        hasher.combine(isInGregorianCalendar)
+    }
+    
 }
 
 // MARK: Comparable
 
 extension Month: Comparable {
-
-  public static func < (lhs: Month, rhs: Month) -> Bool {
-    guard lhs.era == rhs.era else { return lhs.era < rhs.era }
-
-    let lhsCorrectedYear = lhs.isInGregorianCalendar && lhs.era == 0 ? -lhs.year : lhs.year
-    let rhsCorrectedYear = rhs.isInGregorianCalendar && rhs.era == 0 ? -rhs.year : rhs.year
-    guard lhsCorrectedYear == rhsCorrectedYear else { return lhsCorrectedYear < rhsCorrectedYear }
-
-    return lhs.month < rhs.month
-  }
-
+    
+    public static func < (lhs: Month, rhs: Month) -> Bool {
+        guard lhs.era == rhs.era else { return lhs.era < rhs.era }
+        
+        let lhsCorrectedYear = lhs.isInGregorianCalendar && lhs.era == 0 ? -lhs.year : lhs.year
+        let rhsCorrectedYear = rhs.isInGregorianCalendar && rhs.era == 0 ? -rhs.year : rhs.year
+        guard lhsCorrectedYear == rhsCorrectedYear else { return lhsCorrectedYear < rhsCorrectedYear }
+        
+        return lhs.month < rhs.month
+    }
+    
 }
